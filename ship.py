45ee382301f04b337588950fab6e8f6648ba5199
CLaob <16claobena@gmail.com>
2022-03-29 21:10:51 +0100

    Added : - Refactored Keyup and Keydown events - Ship object cannot go over left and right screen - Up - Down

--- ship.py
+++ ship.py
@@ -11,10 +11,13 @@ class Ship():
         self.rect = self.image.get_rect()  # Treat image as a rectangle.
         self.screen_rect = screen.get_rect() # Load image to shape
         self.rect.centerx = self.screen_rect.centerx  # Start at the bottom of the screen
+        self.rect.centery = self.screen_rect.centery  # Start at the bottom of the screen
         self.rect.bottom = self.screen_rect.bottom  # Start at the bottom of the screen
-        self.center = float(self.rect.centerx)
+        self.center = float(self.rect.centerx) # for movement of ship
         self.moving_right = False
         self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
 
 
     def blitme(self):
@@ -23,11 +26,18 @@ class Ship():
 
     def update(self):
         """ Update the ship's position based on the movement flag  """
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right: # check if right edge met
             self.center += self.ai_setting.ship_speed_factor
 
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0: #check if left edge met
             self.center -= self.ai_setting.ship_speed_factor
+
+        if self.moving_up:
+            self.rect.centery -= self.ai_setting.ship_speed_factor
+
+        if self.moving_down:
+            self.rect.centery += self.ai_setting.ship_speed_factor
+
 
         self.rect.centerx = self.center
 
