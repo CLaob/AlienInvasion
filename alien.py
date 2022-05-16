fe4fef4327ff9ff1cd8bc73e3b88dd8dc51620d1
CYBSTER <39672050+CLaob@users.noreply.github.com>
2022-05-16 14:50:45 +0100

    Adding update and movement to aliens
    update function for aliens is added
    check edge method is added

--- alien.py
+++ alien.py
@@ -24,3 +24,16 @@ class Alien(Sprite):
     def blitme(self):
         """Draw alien to the screen"""
         self.screen.blit(self.image, self.rect)
+
+    def check_edge(self):
+        """Return true if alien is hitting edge """
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right:
+            return  True
+        elif self.rect.left <= 0:
+            return  True
+
+    def update(self):
+        """ Moving to the right or left"""
+        self.x += (self.ai_settings.alien_speed_factor * self.ai_settings.fleet_direction)
+        self.rect.x = self.x
